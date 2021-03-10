cdd6c6f9785891bce36bf1342828a3c626233f84
Ryguyrocks22 <70034410+Ryguyrocks22@users.noreply.github.com>
2021-03-10 06:25:29 -0500

    Update 6?

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,7 +6,7 @@ public class GameOver : MonoBehaviour
 
     public Text roundsText;
 
-    private void OnEnable()
+    void OnEnable()
     {
         roundsText.text = PlayerStats.Rounds.ToString();
     }
